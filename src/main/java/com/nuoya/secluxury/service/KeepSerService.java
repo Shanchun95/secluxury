1939c47fee77138862b4c7b39b88cd59b35c482d
quanhan <1217286318@qq.com>
2019-07-23 17:40:32 +0800

    权晗上传了养护服务模块

---
+++ src/main/java/com/nuoya/secluxury/service/KeepSerService.java
@@ -0,0 +1,9 @@
+package com.nuoya.secluxury.service;
+
+import com.nuoya.secluxury.pojo.KeepService;
+
+import java.util.List;
+
+public interface KeepSerService {
+    List<KeepService> getAllService(Integer serviceType);
+}
